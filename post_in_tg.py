80dfa9877c86803c5d5fdb013ae61862f69b8912
AndrPerep <79010353+AndrPerep@users.noreply.github.com>
2021-09-21 21:03:13 +0300

    Update post_in_tg.py
    Исправлены работа с файлом, переменные

--- post_in_tg.py
+++ post_in_tg.py
@@ -5,21 +5,24 @@ import time
 from dotenv import load_dotenv
 from os import listdir 
 from os.path import isfile
-from os.path import join as joinpath
+from os.path import join
 
-def post_in_telegram():
+def post_in_telegram(TG_TOKEN, CHAT_ID, folder):
   bot = telegram.Bot(token=TG_TOKEN)
   sleep_time = 86400
   while True:
-    for picture in get_pictures_list():
-      bot.send_photo(chat_id=CHAT_ID, photo=open(f'images/{picture}', 'rb'))
+    for picture in get_pictures_list(folder):
+      picture_path = f'{folder}{picture}'
+      with open(picture_path, 'rb') as file:
+        bot.send_photo(chat_id=CHAT_ID, photo=open(picture_path, 'rb'))
+      print('hello')
       time.sleep(sleep_time)
 
-def get_pictures_list():
+def get_pictures_list(folder):
   pictures = []
-  path = ('images/')
+  path = (folder)
   for i in listdir(path):
-    if isfile(joinpath(path,i)):
+    if isfile(join(path,i)):
       pictures.append(i)
     else:
       for file in listdir(f'images/{i}/'):
@@ -31,4 +34,6 @@ if __name__ == "__main__":
   TG_TOKEN = os.getenv('TG_TOKEN')
   CHAT_ID = os.getenv('CHAT_ID')
 
-  post_in_telegram()
+  folder = 'images/'
+
+  post_in_telegram(TG_TOKEN, CHAT_ID, folder)
