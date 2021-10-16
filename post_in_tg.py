e5aa7cf75d8706085bf65c8b2472a5abc6e99b9c
AndrPerep <79010353+AndrPerep@users.noreply.github.com>
2021-10-16 17:40:01 +0300

    Убран тип из названия функции

--- post_in_tg.py
+++ post_in_tg.py
@@ -16,14 +16,14 @@ def post_in_telegram(TG_TOKEN, CHAT_ID, folder):
   bot = telegram.Bot(token=TG_TOKEN)
   sleep_time = 86400
   while True:
-    for picture in get_pictures_list(folder):
+    for picture in get_pictures(folder):
       picture_path = f'{folder}{picture}'
       with open(picture_path, 'rb') as file:
         bot.send_photo(chat_id=CHAT_ID, photo=open(picture_path, 'rb'))
       time.sleep(sleep_time)
 
 
-def get_pictures_list(folder):
+def get_pictures(folder):
   pictures = []
   path = (folder)
   for i in listdir(path):
